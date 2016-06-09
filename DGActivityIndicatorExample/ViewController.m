ca7fd76137024bed21ff7d486646083ff58f3f14
iOS-Mayank <Sonitek@Sonitek-Macs-iMac.local>
2016-06-09 18:19:49 +0530

    DGActivityIndicatorView-master

---
+++ DGActivityIndicatorExample/ViewController.m
@@ -0,0 +1,69 @@
+//
+//  ViewController.m
+//  DGActivityIndicatorExample
+//
+//  Created by Danil Gontovnik on 5/23/15.
+//  Copyright (c) 2015 Danil Gontovnik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "DGActivityIndicatorView.h"
+
+@implementation ViewController
+
+#pragma mark -
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:237/255.0f green:85/255.0f blue:101/255.0f alpha:1.0f];
+    
+    NSArray *activityTypes = @[@(DGActivityIndicatorAnimationTypeNineDots),
+                               @(DGActivityIndicatorAnimationTypeTriplePulse),
+                               @(DGActivityIndicatorAnimationTypeFiveDots),
+                               @(DGActivityIndicatorAnimationTypeRotatingSquares),
+                               @(DGActivityIndicatorAnimationTypeDoubleBounce),
+                               @(DGActivityIndicatorAnimationTypeTwoDots),
+                               @(DGActivityIndicatorAnimationTypeThreeDots),
+                               @(DGActivityIndicatorAnimationTypeBallPulse),
+                               @(DGActivityIndicatorAnimationTypeBallClipRotate),
+                               @(DGActivityIndicatorAnimationTypeBallClipRotatePulse),
+                               @(DGActivityIndicatorAnimationTypeBallClipRotateMultiple),
+                               @(DGActivityIndicatorAnimationTypeBallRotate),
+                               @(DGActivityIndicatorAnimationTypeBallZigZag),
+                               @(DGActivityIndicatorAnimationTypeBallZigZagDeflect),
+                               @(DGActivityIndicatorAnimationTypeBallTrianglePath),
+                               @(DGActivityIndicatorAnimationTypeBallScale),
+                               @(DGActivityIndicatorAnimationTypeLineScale),
+                               @(DGActivityIndicatorAnimationTypeLineScaleParty),
+                               @(DGActivityIndicatorAnimationTypeBallScaleMultiple),
+                               @(DGActivityIndicatorAnimationTypeBallPulseSync),
+                               @(DGActivityIndicatorAnimationTypeBallBeat),
+                               @(DGActivityIndicatorAnimationTypeLineScalePulseOut),
+                               @(DGActivityIndicatorAnimationTypeLineScalePulseOutRapid),
+                               @(DGActivityIndicatorAnimationTypeBallScaleRipple),
+                               @(DGActivityIndicatorAnimationTypeBallScaleRippleMultiple),
+                               @(DGActivityIndicatorAnimationTypeTriangleSkewSpin),
+                               @(DGActivityIndicatorAnimationTypeBallGridBeat),
+                               @(DGActivityIndicatorAnimationTypeBallGridPulse),
+                               @(DGActivityIndicatorAnimationTypeRotatingSandglass),
+                               @(DGActivityIndicatorAnimationTypeRotatingTrigons),
+                               @(DGActivityIndicatorAnimationTypeTripleRings),
+                               @(DGActivityIndicatorAnimationTypeCookieTerminator),
+                               @(DGActivityIndicatorAnimationTypeBallSpinFadeLoader)];
+
+    for (int i = 0; i < activityTypes.count; i++) {
+        DGActivityIndicatorView *activityIndicatorView = [[DGActivityIndicatorView alloc] initWithType:(DGActivityIndicatorAnimationType)[activityTypes[i] integerValue] tintColor:[UIColor whiteColor]];
+        CGFloat width = self.view.bounds.size.width / 5.0f;
+        CGFloat height = self.view.bounds.size.height / 7.0f;
+
+        activityIndicatorView.frame = CGRectMake(width * (i % 7), height * (int)(i / 7), width, height);
+        [self.view addSubview:activityIndicatorView];
+        [activityIndicatorView startAnimating];
+    }
+}
+
+@end
---
+++ DGActivityIndicatorView.podspec
@@ -0,0 +1,26 @@
+#
+#  Be sure to run `pod spec lint DGActivityIndicatorView.podspec' to ensure this is a
+#  valid spec and to remove all comments including this before submitting the spec.
+#
+#  To learn more about Podspec attributes see http://docs.cocoapods.org/specification.html
+#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
+#
+
+Pod::Spec.new do |s|
+
+  s.name         = "DGActivityIndicatorView"
+  s.version      = "2.0"
+  s.authors      = { "Danil Gontovnik" => "gontovnik.danil@gmail.com" }
+  s.homepage     = "https://github.com/gontovnik/DGActivityIndicatorView"
+  s.summary      = "DGActivityIndicatorView is a great way to make loading spinners in your application look nicer."
+  s.source       = { :git => "https://github.com/gontovnik/DGActivityIndicatorView.git",
+                     :tag => '2.0' }
+  s.license      = { :type => "MIT", :file => "LICENSE" }
+  s.platform = :ios, '7.0'
+  s.source_files = "DGActivityIndicatorView/**/*.{h,m}"
+
+  s.requires_arc = true
+  
+  s.ios.deployment_target = '7.0'
+  s.ios.frameworks = ['UIKit', 'Foundation']
+end
